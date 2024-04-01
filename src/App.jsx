76dc2d49bbeea391417cece5e1d51fe2169b8b3f
Ammar-Amin <ammaramin5253@gmail.com>
2024-04-01 23:54:44 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='text-4xl font-bold py-2 text-white bg-slate-900 text-center'
+      >Ammar's ToDo List</h1>
+    </>
+  )
+}
+
+export default App
